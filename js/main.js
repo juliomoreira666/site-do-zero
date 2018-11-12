6abd8ef2c2a79a6edfa86af197445f720b0f44c9
Júlio Moreira <julio.moreira666@gmail.com>
2018-11-12 00:04:27 -0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
